09029f9ec597cdfa278b87dd976c01565901d1e0
Ryan Rowland <ryan.w.rowland@gmail.com>
2018-05-08 17:35:49 -0700

    Add root path

--- nimiq-client-install.sh
+++ nimiq-client-install.sh
@@ -5,89 +5,111 @@
 # Flavor: Porky Pool (https://www.porkypool.com)
 ################################################################################
 output() {
-    printf "\E[0;33;40m"
-    echo $1
-    printf "\E[0m"
+  printf "\E[0;33;40m"
+  echo $1
+  printf "\E[0m"
 }
 
 displayErr() {
-    echo
-    echo $1;
-    echo
-    exit 1;
+  echo
+  echo $1;
+  echo
+  exit 1;
 }
 
-    output " "
-    output "Make sure you double check before hitting enter! Only one shot at these!"
-    output "You will find examples in the brackets."
-    output " "
-    read -e -p "Enter the pool URL: " -i "us-east.porkypool.com:8444" POOL
-    read -e -p "Enter the miner CPU threads: " -i $(getconf _NPROCESSORS_ONLN) THREADS
-    read -e -p "Enter your wallet address: " WALLET
-    read -e -p "Enter device name: " EXTRADATA
-    read -e -p "Enter statistics interval in seconds: " -i "10" STATISTICS
-    
-    output " "
-    output "Making sure everything is up to date."
-    output " "
-    sleep 3
-    
-    sudo add-apt-repository -y ppa:deadsnakes/ppa
-    sudo apt-get -y update 
-    sudo apt-get -y upgrade
-    sudo apt-get -y autoremove
-    
-    output " "
-    output "Adding nodejs sources."
-    output " "
-    sleep 3
-    
-    curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash - 
-    
-    output " "
-    output "Installing required dependencies."
-    output " "
-    sleep 3
-    
-    sudo apt-get install -y git build-essential python2.7 python-dev nodejs unzip
-	
-    output " "
-    output "Downloading Nimiq core."
-    output " "
-    sleep 3
+IS_ROOT=false
+if [ "$EUID" -eq 0 ]; then
+  output "WARNING: It looks like you're running as root. It is highly recommended NOT to"
+  output "run miners as root, as you may open yourself to vulnerabilities!"
 
-    git clone https://github.com/nimiq-network/core.git
+  while [[ ! $CONTINUE =~ ^[yn]$ ]]; do
+    read -e -s -n 1 -p "Continue as root anyway? [y/N] " CONTINUE
+    if [ -z "$CONTINUE" ]; then
+      CONTINUE='n'
+    else
+      CONTINUE=$(echo "$CONTINUE" | tr '[:upper:]' '[:lower:]')
+    fi
+  done
 
-    output " "
-    output "Building Nimiq core client."
-    output " "
-    sleep 3
+  if [[ $CONTINUE == "n" ]]; then
+    exit
+  fi
 
-    cd core
-    sudo npm install -g gulp
-    npm install
-    gulp build-node
-    sudo npm install -g node-gyp
-    cd node_modules/node-lmdb
-    node-gyp rebuild
+  IS_ROOT=true
+fi
 
-    output " "
-    output "Building launch scripts."
-    output " "
-    sleep 3
+output " "
+output " "
+output "Please double check before hitting enter! You only have one shot at these!"
+output " "
+read -e -p "Enter the pool URL: " -i "us-east.porkypool.com:8444" POOL
+read -e -p "Enter the miner CPU threads: " -i $(getconf _NPROCESSORS_ONLN) THREADS
+read -e -p "Enter your wallet address: " WALLET
+read -e -p "Enter device label: " EXTRADATA
+read -e -p "Enter statistics interval in seconds: " -i "15" STATISTICS
 
-    cd ../../..
-    echo '#!/bin/bash
-    SCRIPT_PATH=$(dirname "$0")/core
-    $SCRIPT_PATH/clients/nodejs/nimiq "$@"' > miner
-    chmod u+x miner
- 
-    echo '#!/bin/bash
-    UV_THREADPOOL_SIZE='"${THREADS}"' ./miner --dumb --pool='"${POOL}"' --miner='"${THREADS}"' --wallet-address="'"${WALLET}"'" --extra-data="'"${EXTRADATA}"'" --statistics='"${STATISTICS}"'' > start
-    chmod u+x start
+output " "
+output "Making sure everything is up to date."
+output " "
+sleep 3
 
-    output " "
-    output "You can start the miner by typing ./start"
-    output "If you need to change any settings, you can do so inside the ./start file."
-    output " "
-    sleep 3
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y autoremove
+
+output " "
+output "Adding nodejs sources."
+output " "
+sleep 3
+
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+
+output " "
+output "Installing required dependencies."
+output " "
+sleep 3
+
+sudo apt-get install -y git build-essential python2.7 python-dev nodejs unzip
+
+output " "
+output "Downloading Nimiq core."
+output " "
+sleep 3
+
+git clone https://github.com/ryan-rowland/core.git
+
+output " "
+output "Building Nimiq core client."
+output " "
+sleep 3
+
+cd core
+if [ IS_ROOT == true ]; then
+  npm install --unsafe-perm
+else
+  npm install
+fi
+npm run prebuild
+
+output " "
+output "Building launch scripts."
+output " "
+sleep 3
+
+cd ..
+echo '#!/bin/bash
+SCRIPT_PATH=$(dirname "$0")/core
+$SCRIPT_PATH/clients/nodejs/nimiq "$@"' > miner
+chmod u+x miner
+
+echo '#!/bin/bash
+UV_THREADPOOL_SIZE='"${THREADS}"' ./miner --dumb --pool='"${POOL}"' --miner='"${THREADS}"' --wallet-address="'"${WALLET}"'" --extra-data="'"${EXTRADATA}"'" --statistics='"${STATISTICS}"'' > start
+chmod u+x start
+
+output "Congratulations! If everything went well you can now start mining."
+output " "
+output "To start the miner type ./start"
+output " "
+output "If you need to change any settings, you can do so by editing the start file."
+sleep 3
---
+++ nimiq-client-reconfigure.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+################################################################################
+# Author:  Bhlynd
+# Program: Reconfigure Nimiq on Ubuntu
+################################################################################
+output() {
+  printf "\E[0;33;40m"
+  echo $1
+  printf "\E[0m"
+}
+
+displayErr() {
+  echo
+  echo $1;
+  echo
+  exit 1;
+}
+
+output " "
+output "Please double check before hitting enter! You only have one shot at these!"
+output " "
+read -e -p "Enter the pool URL: " -i "us-east.porkypool.com:8444" POOL
+read -e -p "Enter the miner CPU threads: " -i $(getconf _NPROCESSORS_ONLN) THREADS
+read -e -p "Enter your wallet address: " WALLET
+read -e -p "Enter device label: " EXTRADATA
+read -e -p "Enter statistics interval in seconds: " -i "15" STATISTICS
+
+output " "
+output "Building launch script."
+output " "
+sleep 3
+
+echo '#!/bin/bash
+UV_THREADPOOL_SIZE='"${THREADS}"' ./miner --dumb --pool='"${POOL}"' --miner='"${THREADS}"' --wallet-address="'"${WALLET}"'" --extra-data="'"${EXTRADATA}"'" --statistics='"${STATISTICS}"'' > start
+chmod u+x start
+
+output "Congratulations! If everything went well you can now start mining."
+output " "
+output "To start the miner type ./start"
+output " "
+output "If you need to change any settings, you can do so by editing the start file."
+output ""
+output "OR"
+output ""
+output "Run the below command to automate the process of editing the start file."
+output "bash -c "'"$(curl -o - https://raw.githubusercontent.com/Bhlynd/Nimiq-Install-Script/master/nimiq-client-reconfigure.sh)"'""
+output " "
+sleep 3
